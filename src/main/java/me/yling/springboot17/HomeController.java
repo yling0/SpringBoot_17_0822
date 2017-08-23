fd909240c434c3f8112fcd7420e5269ba053fcfe
YLing <github8383>
2017-08-22 21:52:21 -0400

    first commit

---
+++ src/main/java/me/yling/springboot17/HomeController.java
@@ -0,0 +1,23 @@
+package me.yling.springboot17;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index ()
+    {
+        return "index";
+    }
+
+    @RequestMapping("/login")
+    public String login()
+    {
+        return "login";
+    }
+
+
+
+}
